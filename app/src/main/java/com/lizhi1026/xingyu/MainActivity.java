489f0d1302f6cd005bfb5c3fee7188d00d8415c3
lizhi <zhi.li@wz-inc.com>
2021-05-12 14:01:03 +0800

    add project

---
+++ app/src/main/java/com/lizhi1026/xingyu/MainActivity.java
@@ -0,0 +1,14 @@
+package com.lizhi1026.xingyu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
